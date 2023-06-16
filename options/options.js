c1a03b8fbc00c726743a5ecbe4997cbf5e7c44ac
Crowdbotics <engineering@crowdbotics.com>
2023-06-16 08:24:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-dew-dev-92088",
+  url: "https://divine-dew-dev-92088.botics.co",
+  api: "https://divine-dew-dev-92088.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
